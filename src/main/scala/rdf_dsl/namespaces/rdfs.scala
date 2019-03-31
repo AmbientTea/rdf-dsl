271d043e63c81d2b3dea9c5d3215cfbd1d7694be
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2019-03-31 16:27:46 +0200

    more rdfs namespace members

--- src/main/scala/rdf_dsl/namespaces/rdfs.scala
+++ src/main/scala/rdf_dsl/namespaces/rdfs.scala
@@ -1,7 +1,7 @@
 package rdf_dsl.namespaces
 
 import rdf_dsl.entity
-import rdf_dsl.entity.Property
+import rdf_dsl.entity.{Entity, Property}
 import rdf_dsl.namespace.Namespace
 
 object rdfs extends Namespace(
@@ -10,10 +10,27 @@ object rdfs extends Namespace(
   authority = "www.w3.org",
   path = "2000/01/rdf-schema".split("/").toList) {
 
+  object Resource extends entity.Class("Resource")
+
   object Class extends entity.Class("Class")
+
+  object Literal extends entity.Class("Literal")
+
+  object Datatype extends entity.Class("Datatype")
 
   object subClassOf extends Property[entity.Class, entity.Class]("subClassOf")
 
   object subPropertyOf extends Property[Property[_, _], Property[_, _]]("subPropertyOf")
+
+
+  object domain extends Property[Property[_, _], Entity]("domain")
+
+  object range extends Property[Property[_, _], Entity]("range")
+
+  object seeAlso extends Property("seeAlso")
+
+  object label extends Property("label")
+
+  object comment extends Property("comment")
 
 }
